cb9bb56cdf7e603693ebf4f8e3bbfb128c01763e
krag57 <krag57@yahoo.com>
2021-03-17 14:40:10 -0500

    R

--- HW03.R
+++ HW03.R
@@ -30,62 +30,83 @@ Autotest=Auto[tstIndex,]
 #Multi-collinearity
 plot.new()
 splom(Auto, pscales = 0,groups = Auto$mpg01)
+Auto$mpg01<-as.factor(Auto$mpg01)
+Auto$cylinders<-as.factor(Auto$cylinders)
+Auto$year<-as.factor(Auto$year)
 summary(Auto)
 par(mfrow = c(2,3))
 for (i in 2:7) boxplot(Autotrain[,i], xlab=names(Auto)[i])
 dev.off()
 
 #correalation
-cor1=round(cor(Auto[,2:7]),2)
-#write.csv(cor1, file = "corr.csv")
+cor1=round(cor(Auto[,3:6]),2)
+write.csv(cor1, file = "corrA.csv")
 
-ggplot(data = Auto, aes(x = origin, y = weight)) +
+ggplot(data = Auto, aes( origin)) +
+  geom_bar( stat='count') +
+  xlab('Region of Origin') +
+  ylab('Count') +
+  ggtitle('Frequency: Origin') 
+
+p1=ggplot(data = Auto, aes(x = origin, y = weight)) +
   geom_boxplot() +
   xlab('Region of Origin') +
   ylab('Weight') +
-  ggtitle('Weight Comparison by Region of Origin') 
+  ggtitle('Weight Vs. Region of Origin') 
 
-ggplot(data = Auto, aes(x = as.factor(cylinders), y = weight)) +
+p2=ggplot(data = Auto, aes(x = cylinders, y = weight)) +
   geom_boxplot() +
   xlab('Number of Cylinders') +
   ylab('Weight') +
-  ggtitle('Weight Comparison by Number of Cylinders') 
+  ggtitle('Weight Vs. Number of Cylinders') 
 
-ggplot(data = Auto, aes(x = as.factor(mpg01), y = weight)) +
+pixelFigure=ggarrange(plotlist=list(p1,p2),ncol = 2,nrow = 1)
+pixelFigure
+
+
+ggplot(data = Auto, aes(x = mpg01, y = weight)) +
   geom_boxplot() +
-  xlab('Number of Cylinders') +
+  xlab('MPG') +
   ylab('Weight') +
-  ggtitle('Weight Comparison by Number of Cylinders') 
+  ggtitle('MPG by Weight') 
 
 
-ggplot(data = Auto, aes(x =  as.factor(cylinders), fill = origin)) +
+ggplot(data = Auto, aes(x =  cylinders, fill = origin)) +
   geom_bar() +
   xlab('Number of Cylinders') +
   ylab('Count') +
   ggtitle('Cars from Each Region by Number of Cylinders')
 
-ggplot(data = Auto, aes(x =  as.factor(mpg01), fill = origin)) +
+ggplot(data = Auto, aes(x =  mpg01, fill = origin)) +
   geom_bar() +
   xlab('Number of Cylinders') +
   ylab('Count') +
   ggtitle('Cars from Each Region by Number of Cylinders')
 
-ggplot(data = Auto, aes(x = as.factor(year), fill = as.factor(cylinders))) +
+ggplot(data = Auto, aes(x = year, fill = cylinders)) +
   geom_bar() +
   facet_wrap(~ origin, ncol = 1) +
   xlab('Model Year') +
   ylab('Count') +
-  ggtitle('Each Region of Origin\'s Product Mix Over Time')
+  ggtitle('Origin Vs. Products over time')
 
-ggplot(data = Auto, aes(x = as.factor(year), fill = as.factor(mpg01))) +
+ggplot(data = Auto, aes(x = year, fill = mpg01)) +
   geom_bar() +
   facet_wrap(~ origin, ncol = 1) +
   xlab('Model Year') +
   ylab('Count') +
-  ggtitle('Each Region of Origin\'s Product Mix Over Time')
+  ggtitle('MPG Vs. Product Mix Over Time')
 
-ggplot(data = Auto, aes(x = as.factor(cylinders), fill = as.factor(mpg01))) +
+p3=ggplot(data = Auto, aes(x = mpg01, y = weight)) +
+  geom_boxplot() +
+  xlab('MPG') +
+  ylab('Weight') +
+  ggtitle('MPG by Weight') 
+
+p4=ggplot(data = Auto, aes(x = cylinders, fill = mpg01)) +
   geom_bar() +
   xlab('Number of Cylinders') +
   ylab('Count') +
-  ggtitle('Cars from Each Region by Number of Cylinders')
+  ggtitle('MPG by Number of Cylinders')
+pixelFigure2=ggarrange(plotlist=list(p3,p4),ncol = 2,nrow = 1)
+pixelFigure2
